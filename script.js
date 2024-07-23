5581e6ecce73821ef5f324bc0237807ed099b905
Shubhojit saha <139947793+Shubhojit123@users.noreply.github.com>
2024-07-23 17:27:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+
+let apiKey = "9ccf94232b67a0f7021eaf9a142d7af6";
+let api = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+let searchBox = document.querySelector(".search input");
+let btn = document.querySelector(".search button");
+let weatherImg = document.querySelector(".weatherImg");
+console.log(searchBox.value);
+
+
+async function weather(city){
+    const response = await fetch(api + city + `&appid=${apiKey}`);
+    if(response.status == 404 || response.status == 400)
+    {
+        document.querySelector(".city").innerHTML = "Enter Valid City"
+        document.querySelector(".city").style.fontSize = "30px";
+    document.querySelector(".temp").innerHTML = "0°c";
+    document.querySelector(".wind").innerHTML = "0"+" "+"km/h";
+    document.querySelector(".humadites").innerHTML ="0%";
+    document.querySelector(".feel").innerHTML = "Feels like"+" "+"0°c";
+
+    }
+    else
+    {
+    var data = await response.json();
+    console.log(data);
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp)+"°c";
+    document.querySelector(".wind").innerHTML = data.wind.speed+"km/h";
+    document.querySelector(".humadites").innerHTML = data.main.humidity+"%";
+    document.querySelector(".feel").innerHTML = "Feels like"+" "+Math.round(data.main.feels_like)+"°c";
+    if(data.weather[0].main == "clouds")
+    {
+        weatherImg.src = "Images/overcast.png";
+    }
+    else if (data.weather[0].main == "clear")
+    {
+        weatherImg.src = "Images/clear.png";
+    }
+    else if (data.weather[0].main == "Rain")
+    {
+        weatherImg.src = "Images/rain.png";
+    }
+    else 
+    {
+        weatherImg.src = "Images/overcast.png";
+    }
+}
+}
+btn.addEventListener("click",()=>{
+    let lowerCaseValue = searchBox.value.toLowerCase();
+
+    weather(lowerCaseValue);
+
+})
+
